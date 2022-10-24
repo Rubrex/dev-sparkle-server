1895e9b2c9d4ec215f0f7e2e2cb12a60cadb8cf1
Rubel Hossain <rubrex96@hotmail.com>
2022-10-25 02:25:56 +0600

    installed dotenv for .env

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+
+const port = process.env.PORT || 5000;
+
 const app = express();
+
+// Disable cors
+app.use(cors());
+
+// app is listening on port
+app.listen();
